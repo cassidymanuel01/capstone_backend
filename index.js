f92e3d28bc628b47a3c2a03fc7899a8c70686ae4
cassidymanuel01 <cassidymanuel.59@gmail.com>
2022-08-30 12:01:17 +0200

    changess

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const port = parseInt(process.env.PORT) || 3000;
 
 app.use(router, cors(), express.json(), bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('views'));
-
 app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
@@ -20,6 +18,8 @@ app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
+
+app.use(express.static('views'));
 
 app.listen(port, ()=> {console.log(`Server is running on port ${port}`)});
 
