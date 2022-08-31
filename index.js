18fd2b4193ef47c4378c461e998662e364728bb8
cassidymanuel01 <cassidymanuel.59@gmail.com>
2022-08-31 08:35:19 +0200

    changess

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
-
   next();
 });
 app.use(
@@ -25,7 +24,7 @@ app.use(
   })
 );
 
-app.use(router, express.json(), express.urlencoded({ extended: true }));
+app.use(router, express.json(), router.urlencoded({ extended: true }));
 
 app.use(express.static("views"));
 
