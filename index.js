1d62b6fc4228c6eb3465517b03ff37bea1d3f605
cassidymanuel01 <cassidymanuel.59@gmail.com>
2022-08-31 08:43:46 +0200

    changess

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const port = parseInt(process.env.PORT) || 3000;
 
-// app.use((req, res, next) => {
-//   res.setHeader("mode", "no-cors");
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Headers", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "*");
-//   next();
-// });
-// app.use(
-//   cors({
-//     mode: "no-cors",
-//     origin: ["http://192.168.8.169:8080", "http://localhost:8080"],
-//     credentials: true,
-//   })
-// );
+app.use((req, res, next) => {
+  res.setHeader("mode", "no-cors");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  next();
+});
+app.use(
+  cors({
+    mode: "no-cors",
+    origin: ["http://192.168.8.169:8080", "http://localhost:8080"],
+    credentials: true,
+  })
+);
 
 app.use(router, express.json(), express.urlencoded({ extended: true }));
 
